dae442aaf873637d05627e2bfcaa5adbbe391d6f
BrantLauro <brantlauro@gmail.com>
2024-04-30 18:05:09 -0300

    FornecedorController atualizado para edição dos registros

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,28 +8,22 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $fornecedores = Fornecedor::all();
         return view('app.fornecedor.listar', compact('fornecedores'));
     }
 
+    public function pesquisa(Request $request) {
+        if(empty($request->query())) {
+            return view('app.fornecedor.pesquisa');
+        }else{
+            $fornecedores = Fornecedor::search($request)->get();
+            return view('app.fornecedor.listar', compact('fornecedores'));
+        }
+    }
+
     public function novo() {
         return view("app.fornecedor.novo");
-    }
-
-    public function pesquisa() {
-        return view("app.fornecedor.pesquisa");
-    }
-
-    public function listar(Request $request) {
-        $fornecedores = Fornecedor::where("nome", 'like' ,'%'.$request->nome.'%')
-        ->where("site", 'like' ,'%'.$request->site.'%')
-        ->where("uf", 'like' ,'%'.$request->uf.'%')
-        ->where("email", 'like' ,'%'.$request->email.'%');
-       // ->get();
-        dd($fornecedores);
-        //return redirect()->back()->with('message',''.$request->id.' '.$request->nome.' '.$request->email.' '. $request->site. " ". $request->uf);
-        //return view('app.fornecedor.listar', compact('fornecedores'));
     }
 
     public function salvar(Request $request) {
@@ -61,5 +55,35 @@ class FornecedorController extends Controller
         $fornecedor->delete();
 
         return redirect()->route('app.fornecedores')->with('message', 'Deletado com sucesso!');
+    }
+
+    public function update(Request $request, $id) {
+        $fornecedor = Fornecedor::findOrFail($id);
+        if($request->method() == 'GET') {
+            return view('app.fornecedor.editar', compact('fornecedor'));
+        } else {
+            $regras = [
+                'nome' => 'required|min:3|max:50|unique:fornecedores',
+                'site' => 'max:30',
+                'uf' => 'max: 2',
+                'email'=> 'required|max:80|email',
+            ];
+    
+            $mensagens = [
+                'required' => 'O campo :attribute é obrigatório.',
+                'nome.min' => 'O campo nome tem que ter pelo menos 3 caracteres.',
+                'nome.max' => 'O campo nome pode ter no máximo 50 caracteres.',
+                'nome.unique' => 'Fornecedor já cadastrado',
+                'site.max' => 'O campo site pode ter no máximo 30 caracteres.',
+                'uf.max' => 'O campo uf pode ter no máximo 2 caracteres.',
+                'email.email' => 'Insira um email valido.',
+                'email.max' => 'O campo email pode ter no máximo 80 caracteres.'
+            ];
+    
+            $request->validate($regras, $mensagens);
+            $fornecedor->update($request->all());
+            return redirect()->route('app.fornecedores')->with('message', 'Fornecedor atualizado com sucesso!');
+        }
+
     }
 }
